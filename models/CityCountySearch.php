28ecadef27696c9f953c9aa973f7aa7ce702b5df
JohnBrooking <johnbrooking4@gmail.com>
2020-03-03 09:17:42 -0500

    Corrected capitalization in CityCountySearch

--- models/CityCountySearch.php
+++ models/CityCountySearch.php
@@ -4,7 +4,7 @@ namespace app\models;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\Models\CityCounty;
+use app\models\CityCounty;
 
 /**
  * CityCountySearch represents the model behind the search form of `app\Models\CityCounty`.
