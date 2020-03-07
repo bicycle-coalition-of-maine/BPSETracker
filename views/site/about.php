bcdfd31e7339e784dbbf3b211b1667e581bd87a0
JohnBrooking <johnbrooking4@gmail.com>
2020-03-07 17:21:13 -0500

    Update version on About screen
    This should go with release v1.2.3, but forgot to change it until after the release had already been created.

--- views/site/about.php
+++ views/site/about.php
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class='col-sm-10'>
             <h1 style='text-align: center;'><?= Html::encode($this->title) ?></h1>
             <h2 style='text-align: center;'>GitHub Release 
-                <a href='https://github.com/bicycle-coalition-of-maine/BPSETracker/releases/tag/v1.2.2' target='_blank'>v1.2.2</a>
+                <a href='https://github.com/bicycle-coalition-of-maine/BPSETracker/releases/tag/v1.2.3' target='_blank'>v1.2.3</a>
             </h2>
         </div>
     </div>
